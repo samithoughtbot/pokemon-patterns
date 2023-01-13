9e3a73489d940686a7fb9a1d23865e166d5e923f
sami <sami@thoughtbot.com>
2023-01-13 10:46:33 +0000

    evolve a pokemon

---
+++ spec/features/user_evolves_a_pokemon_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "user evolves a pokemon" do
+  scenario "evolves a pokemon every time the user clicks the evolve button" do
+    first_pokemon = Pokemon.create(name: "Charmander", kind: "Fire", evolution: 1)
+    visit pokemons_path
+    
+    click_button "Evolve"
+    
+    expect(page).to have_content("Pokemon successfully evolved")
+    expect(page).to have_content("Evolution: 2")
+
+    click_button "Evolve"
+    
+    expect(page).to have_content("Pokemon successfully evolved")
+    expect(page).to have_content("Evolution: 3")
+
+    click_button "Evolve"
+    
+    expect(page).to have_content("Pokemon successfully evolved")
+    expect(page).to have_content("Evolution: 4")
+  end
+end
