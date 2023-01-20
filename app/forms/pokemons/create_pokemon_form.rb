dd6b7cd9da70e8465656b209b92bbc58a3dd4301
sami <sami@thoughtbot.com>
2023-01-20 12:11:28 +0000

    Only allow creation of original 150 pokemon
    This PR ensures that when a User creates a pokemon by typing in their
    name, they can only create a pokemon that was part of the original
    150.
    
    In order to achieve this the codebase needs to know about the
    original 150. I have encapsulated this data into a PORO, that I will
    then be able to use in the future for other validations such as making
    sure when a pokemon evolves it will evolve into the correct pokemon.

--- app/forms/pokemons/create_pokemon_form.rb
+++ app/forms/pokemons/create_pokemon_form.rb
@@ -4,6 +4,10 @@ class Pokemons::CreatePokemonForm
   attr_accessor :name, :kind
 
   validates :name, :kind, presence: true
+  validates :name, inclusion: { 
+    in: OriginalPokemon::NAMES, 
+    message: "must be one of the original 150 pokemon" 
+  }
 
   def save
     return false if invalid?
@@ -11,4 +15,5 @@ class Pokemons::CreatePokemonForm
     pokemon = Pokemon.new(name: name, kind: kind)
     pokemon.save!
   end
+
 end
