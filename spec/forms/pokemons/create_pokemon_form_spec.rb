bc65b94d6993c98a82d1eadca2a6eabfdeb8152c
sami <sami@thoughtbot.com>
2023-02-03 13:28:27 +0000

    only create pokemone at level 1 stage

--- spec/forms/pokemons/create_pokemon_form_spec.rb
+++ spec/forms/pokemons/create_pokemon_form_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe Pokemons::CreatePokemonForm, type: :model do
     it { should validate_presence_of(:kind) }
 
     it 'is one of the original 150 pokemon' do
-      stub_const("OriginalPokemon::NAMES", ["Charizard", "Mew"])
-      # pokemon = instance_double(
-      #   Pokemon, 
-      #   is_original_150?: false)
+      pokemon = instance_double(
+        Pokemon, 
+        is_original_150?: false,
+        evolution_stage: 1
+      )
+      allow(Pokemon).to receive(:new).and_return(pokemon)
       params = { name: "Pingu", kind: "Fire" }
-      # allow(pokemon).to receive(:is_original_150?).and_return(false)
-      # allow_any_instance_of(Pokemon).to receive(:is_original_150?).and_return(false)
       create_pokemon_form = Pokemons::CreatePokemonForm.new(params)
 
       create_pokemon_form.save
@@ -21,17 +21,34 @@ RSpec.describe Pokemons::CreatePokemonForm, type: :model do
         create_pokemon_form.errors[:name]
       ).to eq(["must be one of the original 150 pokemon"])
     end
+
+    it 'is at stage 1 of evolvution' do
+      pokemon = instance_double(
+        Pokemon, 
+        is_original_150?: true,
+        evolution_stage: 2
+      )
+      allow(Pokemon).to receive(:new).and_return(pokemon)
+      params = { name: "Charizard", kind: "Fire" }
+      create_pokemon_form = Pokemons::CreatePokemonForm.new(params)
+
+      create_pokemon_form.save
+      
+      expect(
+        create_pokemon_form.errors[:name]
+      ).to eq(["you can only create a pokemon at stage 1 of evolution"])
+    end
   end
 
   describe '#save' do
     it 'saves a pokemon to the database' do
-      params = { name: "Charizard", kind: "Fire" }
+      params = { name: "Charmander", kind: "Fire" }
       create_pokemon_form = Pokemons::CreatePokemonForm.new(params)
 
       create_pokemon_form.save
 
       expect(Pokemon.count).to eq(1)
-      expect(Pokemon.last.name).to eq("Charizard")
+      expect(Pokemon.last.name).to eq("Charmander")
       expect(Pokemon.last.kind).to eq("Fire")
     end
   end
--- spec/models/original_pokemon_spec.rb
+++ spec/models/original_pokemon_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OriginalPokemon do
   describe '#is_original_150?' do
     context "when the pokemon is in the original 150" do
       it 'returns true' do
-        stub_const("OriginalPokemon::NAMES", ["Charizard", "Mew"])
+        stub_const("OriginalPokemon::POKEDEX", [{1 => "Charizard", 2 => "Mew" }])
         pokemon = Pokemon.new(name: "Mew", kind: "Fire")
         
         expect(pokemon.is_original_150?).to eq true
@@ -13,11 +13,20 @@ RSpec.describe OriginalPokemon do
 
     context "when the pokemon is not in the original 150" do
       it 'returns false' do
-        stub_const("OriginalPokemon::NAMES", ["Charizard", "Mew"])
+        stub_const("OriginalPokemon::POKEDEX", [{1 => "Charizard", 2 => "Mew" }])
         pokemon = Pokemon.new(name: "Pingu", kind: "Fire")
         
         expect(pokemon.is_original_150?).to eq false
       end
+    end
+  end
+
+  describe '#evolution_stage' do
+    it 'returns evolution stage of the pokemon' do
+      stub_const("OriginalPokemon::POKEDEX", [{1 => "Charizard", 2 => "Mew" }])
+      pokemon = Pokemon.new(name: "Mew", kind: "Fire")
+      
+      expect(pokemon.evolution_stage).to eq 2
     end
   end
 end
