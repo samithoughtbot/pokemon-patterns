2cce12802e578b363ceb17cf4bf8b89ab13eb709
sami <sami@thoughtbot.com>
2023-06-09 10:19:01 +0100

    wip

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,8 @@
 class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all.order(:name)
+    # @pokemons = PresenterGenerator.generate(active_record_collection)
+    # returns an array of presenter objects for the given collection
   end
 
   def new
@@ -18,7 +20,7 @@ class PokemonsController < ApplicationController
   end
 
   private
-  
+
   def pokemon_params
     params.require(:pokemons_create_pokemon_form).permit(:name, :kind)
   end
